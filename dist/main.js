34ed002b8117ef5dd44fa4c7858d54695ffcab1f
DiyorbekAbdulhamidov <drkoffical001@gmail.com>
2023-05-23 14:56:28 +0500

    finish codes

--- dist/main.js
+++ dist/main.js
@@ -9,29 +9,41 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const section = document.querySelector('section');
+const info = document.querySelector('.info');
+const btn = document.querySelector('.toSayt');
+const infoId = document.querySelector('.infoId');
+const infoName = document.querySelector('.infoName');
+const infoUserName = document.querySelector('.infoUserName');
+const infoEmail = document.querySelector('.infoEmail');
+const infoAddress = document.querySelector('.infoAddress');
+const infoPhone = document.querySelector('.infoPhone');
+const infoWebsite = document.querySelector('.infoWebsite');
+const infoCompany = document.querySelector('.infoCompany');
+const animations = document.querySelector(".lds-facebook");
+let table;
 function renderFunction(data) {
-    let table = document.createElement('table');
+    table = document.createElement('table');
     table.style.border = '1px solid black';
     table.style.borderCollapse = 'collapse';
-    let trHeader = document.createElement('tr');
+    let trH = document.createElement('tr');
     let idTh = document.createElement('th');
     let nameTh = document.createElement('th');
     let emailTh = document.createElement('th');
-    let addressTh = document.createElement('th');
+    let adrssTh = document.createElement('th');
     let actionTh = document.createElement('th');
     idTh.classList.add('id');
     actionTh.classList.add('actions');
     idTh.textContent = 'ID';
     nameTh.textContent = 'NAME';
     emailTh.textContent = 'EMAIL';
-    addressTh.textContent = 'ADDRESS';
+    adrssTh.textContent = 'ADDRESS';
     actionTh.textContent = 'ACTIONS';
-    trHeader.appendChild(idTh);
-    trHeader.appendChild(nameTh);
-    trHeader.appendChild(emailTh);
-    trHeader.appendChild(addressTh);
-    trHeader.appendChild(actionTh);
-    table.appendChild(trHeader);
+    trH.appendChild(idTh);
+    trH.appendChild(nameTh);
+    trH.appendChild(emailTh);
+    trH.appendChild(adrssTh);
+    trH.appendChild(actionTh);
+    table.appendChild(trH);
     for (let i = 0; i < data.length; i++) {
         let tr = document.createElement('tr');
         let tdId = document.createElement('td');
@@ -40,11 +52,56 @@ function renderFunction(data) {
         let tdAddress = document.createElement('td');
         let tdAction = document.createElement('td');
         let spanDelete = document.createElement('span');
-        let spanSymptoms = document.createElement('span');
+        let spanKorish = document.createElement('span');
         spanDelete.className = 'material-symbols-outlined delete';
-        spanSymptoms.className = 'material-symbols-outlined eye';
+        spanKorish.className = 'material-symbols-outlined eye';
         spanDelete.textContent = 'delete';
-        spanSymptoms.textContent = 'symptoms';
+        spanKorish.textContent = 'symptoms';
+        spanDelete.addEventListener('click', function () {
+            table.removeChild(tr);
+        });
+        spanKorish.addEventListener('click', function () {
+            let id = tdId.textContent;
+            table.style.display = 'none';
+            info.style.display = 'flex';
+            btn.addEventListener('click', function () {
+                info.style.display = 'none';
+                table.style.display = 'block';
+            });
+            function infos(data) {
+                infoId.textContent = data.id;
+                infoName.textContent = data.name;
+                infoUserName.textContent = data.username;
+                infoEmail.textContent = data.email;
+                infoAddress.textContent = data.address.city;
+                infoPhone.textContent = data.phone;
+                infoWebsite.textContent = data.website;
+                infoCompany.textContent = data.company.name;
+            }
+            function getDataFromAPI() {
+                return __awaiter(this, void 0, void 0, function* () {
+                    info.style.display = "none";
+                    animations.style.display = "inline-block";
+                    try {
+                        const response = yield fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+                        if (response) {
+                            const data = yield response.json();
+                            info.style.display = "flex";
+                            animations.style.display = "none";
+                            // console.log(data);
+                            return data;
+                        }
+                    }
+                    catch (err) {
+                        alert('Error❌');
+                    }
+                });
+            }
+            getDataFromAPI()
+                .then((data) => {
+                infos(data);
+            });
+        });
         tdId.textContent = data[i].id.toString();
         if (parseInt(tdId.innerText) % 2 === 0) {
             tdId.style.backgroundColor = 'rgb(225, 225, 225)';
@@ -62,17 +119,21 @@ function renderFunction(data) {
         tr.appendChild(tdAddress);
         tr.appendChild(tdAction);
         tdAction.appendChild(spanDelete);
-        tdAction.appendChild(spanSymptoms);
+        tdAction.appendChild(spanKorish);
         table.appendChild(tr);
     }
     section === null || section === void 0 ? void 0 : section.appendChild(table);
 }
 function getDataFromAPI() {
     return __awaiter(this, void 0, void 0, function* () {
+        // table.style.display = 'none';
+        // animations.style.display = 'inline-block';
         try {
             const response = yield fetch('https://jsonplaceholder.typicode.com/users');
             if (response) {
                 const data = yield response.json();
+                // table.style.display = "block";
+                // animations.style.display = "none";
                 return data;
             }
         }
--- public/main.ts
+++ public/main.ts
@@ -1,15 +1,26 @@
-const section = document.querySelector('section');
-
+const section = document.querySelector('section')
+const info = document.querySelector('.info') as HTMLDivElement;
+const btn = document.querySelector('.toSayt') as HTMLButtonElement;
+const infoId = document.querySelector('.infoId') as HTMLSpanElement;
+const infoName = document.querySelector('.infoName') as HTMLSpanElement;
+const infoUserName = document.querySelector('.infoUserName') as HTMLSpanElement;
+const infoEmail = document.querySelector('.infoEmail') as HTMLSpanElement;
+const infoAddress = document.querySelector('.infoAddress') as HTMLSpanElement;
+const infoPhone = document.querySelector('.infoPhone') as HTMLSpanElement;
+const infoWebsite = document.querySelector('.infoWebsite') as HTMLSpanElement;
+const infoCompany = document.querySelector('.infoCompany') as HTMLSpanElement;
+const animations = document.querySelector(".lds-facebook") as HTMLDivElement
+let table: HTMLTableElement;
 function renderFunction(data: any) {
-  let table = document.createElement('table');
+  table = document.createElement('table');
   table.style.border = '1px solid black';
   table.style.borderCollapse = 'collapse';
 
-  let trHeader = document.createElement('tr');
+  let trH = document.createElement('tr');
   let idTh = document.createElement('th');
   let nameTh = document.createElement('th');
   let emailTh = document.createElement('th');
-  let addressTh = document.createElement('th');
+  let adrssTh = document.createElement('th');
   let actionTh = document.createElement('th');
 
   idTh.classList.add('id');
@@ -18,16 +29,16 @@ function renderFunction(data: any) {
   idTh.textContent = 'ID';
   nameTh.textContent = 'NAME';
   emailTh.textContent = 'EMAIL';
-  addressTh.textContent = 'ADDRESS';
+  adrssTh.textContent = 'ADDRESS';
   actionTh.textContent = 'ACTIONS';
 
-  trHeader.appendChild(idTh);
-  trHeader.appendChild(nameTh);
-  trHeader.appendChild(emailTh);
-  trHeader.appendChild(addressTh);
-  trHeader.appendChild(actionTh);
+  trH.appendChild(idTh);
+  trH.appendChild(nameTh);
+  trH.appendChild(emailTh);
+  trH.appendChild(adrssTh);
+  trH.appendChild(actionTh);
 
-  table.appendChild(trHeader);
+  table.appendChild(trH);
 
   for (let i = 0; i < data.length; i++) {
     let tr = document.createElement('tr');
@@ -37,12 +48,63 @@ function renderFunction(data: any) {
     let tdAddress = document.createElement('td');
     let tdAction = document.createElement('td');
     let spanDelete = document.createElement('span');
-    let spanSymptoms = document.createElement('span');
+    let spanKorish = document.createElement('span');
 
     spanDelete.className = 'material-symbols-outlined delete';
-    spanSymptoms.className = 'material-symbols-outlined eye';
+    spanKorish.className = 'material-symbols-outlined eye';
     spanDelete.textContent = 'delete';
-    spanSymptoms.textContent = 'symptoms';
+    spanKorish.textContent = 'symptoms';
+
+    spanDelete.addEventListener('click', function () {
+      table.removeChild(tr);
+    });
+
+    spanKorish.addEventListener('click', function () {
+      let id = tdId.textContent;
+
+      table.style.display = 'none';
+      info.style.display = 'flex';
+      btn.addEventListener('click', function () {
+        info.style.display = 'none';
+        table.style.display = 'block';
+
+      });
+      function infos(data: any) {
+        infoId.textContent = data.id;
+        infoName.textContent = data.name;
+        infoUserName.textContent = data.username;
+        infoEmail.textContent = data.email;
+        infoAddress.textContent = data.address.city
+        infoPhone.textContent = data.phone;
+        infoWebsite.textContent = data.website;
+        infoCompany.textContent = data.company.name
+      }
+
+      async function getDataFromAPI() {
+        info.style.display = "none";
+        animations.style.display = "inline-block";
+        try {
+          const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+          if (response) {
+            const data = await response.json();
+            info.style.display = "flex";
+            animations.style.display = "none";
+            // console.log(data);
+            return data;
+          }
+
+        }
+        catch (err) {
+          alert('Error❌')
+        }
+      }
+
+      getDataFromAPI()
+        .then((data) => {
+          infos(data);
+        });
+    }
+    );
 
     tdId.textContent = data[i].id.toString();
     if (parseInt(tdId.innerText) % 2 === 0) {
@@ -62,7 +124,7 @@ function renderFunction(data: any) {
     tr.appendChild(tdAddress);
     tr.appendChild(tdAction);
     tdAction.appendChild(spanDelete);
-    tdAction.appendChild(spanSymptoms);
+    tdAction.appendChild(spanKorish);
 
     table.appendChild(tr);
   }
@@ -71,16 +133,20 @@ function renderFunction(data: any) {
 }
 
 async function getDataFromAPI() {
+  // table.style.display = 'none';
+  // animations.style.display = 'inline-block';
+
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
-
     if (response) {
       const data = await response.json();
+      // table.style.display = "block";
+      // animations.style.display = "none";
       return data;
     }
 
   }
-  catch(err){
+  catch (err) {
     alert('Error❌')
   }
 }
